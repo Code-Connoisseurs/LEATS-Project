a46c5be46d8b397a533f23733a32c5e3a009b1f4
Khayeh <Khayeh@DESKTOP-D4I9DRJ>
2020-08-02 12:47:47 +0200

    Add project files.

---
+++ LEATS Project/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LEATS_Project.MvcApplication" Language="C#" %>
---
+++ LEATS Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LEATS_Project.Startup))]
+namespace LEATS_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
