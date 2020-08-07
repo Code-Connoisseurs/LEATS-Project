80962e03816f660b6bdadeb1c515f940f4dcffbb
Ndumiso <217050448@dstu.ukzn.ac.za>
2020-08-07 05:47:05 +0200

    Commit

---
+++ LEATS Project/Controllers/TutorController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LEATS_Project.Controllers
+{
+    public class TutorController : Controller
+    {
+        // GET: Tutor
+
+        public ActionResult RegisterTutor()
+        {
+            return View();
+        }
+    }
+}
