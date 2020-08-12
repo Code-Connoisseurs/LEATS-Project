7fc5513e9822b95abc41d2a80edc075a41c033fe
Ndumiso <217050448@dstu.ukzn.ac.za>
2020-08-13 00:09:58 +0200

    Set

---
+++ LEATS Project/Controllers/ModuleRequestsController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LEATS_Project.Models;
+
+namespace LEATS_Project.Controllers
+{
+    public class ModuleRequestsController : Controller
+    {
+        private hon06Entities2 db = new hon06Entities2();
+
+        // GET: ModuleRequests
+        public ActionResult Index()
+        {
+            var moduleRequests = db.ModuleRequests.Include(m => m.Student);
+            return View(moduleRequests.ToList());
+        }
+
+        // GET: ModuleRequests/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ModuleRequest moduleRequest = db.ModuleRequests.Find(id);
+            if (moduleRequest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(moduleRequest);
+        }
+
+        // GET: ModuleRequests/Create
+        public ActionResult Create()
+        {
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "Id");
+            return View();
+        }
+
+        // POST: ModuleRequests/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RequestID,StudentID,ModuleCode,ModuleName,RequestDate")] ModuleRequest moduleRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ModuleRequests.Add(moduleRequest);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "Id", moduleRequest.StudentID);
+            return View(moduleRequest);
+        }
+
+        // GET: ModuleRequests/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ModuleRequest moduleRequest = db.ModuleRequests.Find(id);
+            if (moduleRequest == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "Id", moduleRequest.StudentID);
+            return View(moduleRequest);
+        }
+
+        // POST: ModuleRequests/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RequestID,StudentID,ModuleCode,ModuleName,RequestDate")] ModuleRequest moduleRequest)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(moduleRequest).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.StudentID = new SelectList(db.Students, "StudentID", "Id", moduleRequest.StudentID);
+            return View(moduleRequest);
+        }
+
+        // GET: ModuleRequests/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ModuleRequest moduleRequest = db.ModuleRequests.Find(id);
+            if (moduleRequest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(moduleRequest);
+        }
+
+        // POST: ModuleRequests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            ModuleRequest moduleRequest = db.ModuleRequests.Find(id);
+            db.ModuleRequests.Remove(moduleRequest);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LEATS Project/Controllers/SpecialisationsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using LEATS_Project.Models;
+
+namespace LEATS_Project.Controllers
+{
+    public class SpecialisationsController : Controller
+    {
+        private hon06Entities2 db = new hon06Entities2();
+
+        // GET: Specialisations
+        public ActionResult Index()
+        {
+            var specialisations = db.Specialisations.Include(s => s.Module).Include(s => s.Tutor);
+            return View(specialisations.ToList());
+        }
+
+        // GET: Specialisations/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Specialisation specialisation = db.Specialisations.Find(id);
+            if (specialisation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(specialisation);
+        }
+
+        // GET: Specialisations/Create
+        public ActionResult Create()
+        {
+            ViewBag.ModuleID = new SelectList(db.Modules, "ModuleID", "ModuleCode");
+            ViewBag.TutorID = new SelectList(db.Tutors, "TutorID", "Experience");
+            return View();
+        }
+
+        // POST: Specialisations/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SpecialisationID,ModuleID,TutorID")] Specialisation specialisation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Specialisations.Add(specialisation);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ModuleID = new SelectList(db.Modules, "ModuleID", "ModuleCode", specialisation.ModuleID);
+            ViewBag.TutorID = new SelectList(db.Tutors, "TutorID", "Experience", specialisation.TutorID);
+            return View(specialisation);
+        }
+
+        // GET: Specialisations/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Specialisation specialisation = db.Specialisations.Find(id);
+            if (specialisation == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ModuleID = new SelectList(db.Modules, "ModuleID", "ModuleCode", specialisation.ModuleID);
+            ViewBag.TutorID = new SelectList(db.Tutors, "TutorID", "Experience", specialisation.TutorID);
+            return View(specialisation);
+        }
+
+        // POST: Specialisations/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SpecialisationID,ModuleID,TutorID")] Specialisation specialisation)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(specialisation).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ModuleID = new SelectList(db.Modules, "ModuleID", "ModuleCode", specialisation.ModuleID);
+            ViewBag.TutorID = new SelectList(db.Tutors, "TutorID", "Experience", specialisation.TutorID);
+            return View(specialisation);
+        }
+
+        // GET: Specialisations/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Specialisation specialisation = db.Specialisations.Find(id);
+            if (specialisation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(specialisation);
+        }
+
+        // POST: Specialisations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Specialisation specialisation = db.Specialisations.Find(id);
+            db.Specialisations.Remove(specialisation);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
