7c88523a0c56c75ed7fe8408f45c700628b858cc
Ndumiso <Ndumiso@NDUMISO>
2020-08-31 08:31:54 +0200

    Profile

--- LEATS Project/Controllers/AccountController.cs
+++ LEATS Project/Controllers/AccountController.cs
@@ -206,7 +206,7 @@ namespace LEATS_Project.Controllers
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
-                    await UserManager.AddToRoleAsync(user.Id, "User");
+                    UserManager.AddToRole(user.Id, "User");
                     return RedirectToAction("Login", "Account");
                 }
                 AddErrors(result);
